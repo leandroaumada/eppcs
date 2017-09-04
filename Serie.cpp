4fe6b94416fd594418d24aee1b2021e78b3c93e9
Vini-Freire <vini.freire10@gmail.com>
2017-09-04 16:13:33 -0300

    Update Serie.cpp

--- Serie.cpp
+++ Serie.cpp
@@ -1,1 +1,25 @@
 #include "Serie.h"
+
+
+void setNome(string nome){
+  this->Nome = nome; 
+}
+
+void setNomeDoCanalX(string nomeDoCanalX){
+  this->NomeDoCanalX = nomeDoCanalX; {
+    
+  }
+v
+setNomeDoCanalY(string nomeDoCanalY){
+  this->NomeDoCanalY = nomeDoCanalY; 
+}
+
+string getNome(){
+  return Nome;
+}
+string getNomeDoCanalX(){
+  return NomeDoCanalX;
+}
+string getNomeDoCanalY(){
+  return NomeDoCanalY;
+}
