bd5e44ca8ed5c171f24403a23f4eaadffce40e21
leandroaumada <31452718+leandroaumada@users.noreply.github.com>
2017-09-13 17:44:25 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -10,64 +10,69 @@
 using namespace std;
 
 int main() {
+
+  int i = 0;
   InterfaceSerial* is = new InterfaceSerial();
   is->inicializar(COMM);
-  
+
 
   // Obtem o nome e o atribui a serie ser
   string nomeSerie;
   cout << "Informe o nome da serie: ";
   cin >> nomeSerie;
   cout << endl;
-  
+
   // Obtem  os canais escolhidos
   string canalX;
   string canalY;
   int quantidadeCanais = is->getQuantidadeDeCanais();
   int escolha;
-  
+
   //Imprime a lista de canais para X
+  cout << "Escolha o Canal X: " << endl;
   for(i=0; i < quantidadeCanais; i++){
-    cout << i + 1 << ") " << getNomeDosCanais()[i] << endl;
+    cout << i + 1 << ") " << is->getNomeDosCanais()[i] << endl;
   }
   cin >> escolha;
-  canalX = getNomeDosCanais()[escolha - 1];
-  
+  canalX = is->getNomeDosCanais()[escolha - 1];
+
   //Imprime a lista de canais para Y
+  cout << "Escolha o Canal Y: " << endl;
   for(i=0; i < quantidadeCanais; i++){
-    cout << i + 1 << ") " << getNomeDosCanais()[i] << endl;
+    cout << i + 1 << ") " << is->getNomeDosCanais()[i] << endl;
   }
   cin >> escolha;
-  canalY = getNomeDosCanais()[escolha - 1];
-  
+  canalY = is->getNomeDosCanais()[escolha - 1];
+
   // Obtem o numero de Pontos a adicionar
   int quantidade;
   cout << "Obter quantos pontos? ";
   cin >> quantidade;
-  cout << endl;
+  cout << endl << endl;
 
   // Cria a Serie
   Serie* ser = new Serie();
   ser->setNome(nomeSerie);
 
   cout << "Obtendo os pontos" << endl;
-  for (int i = 0; i < quantidade; i++) {
+  for (i = 0; i < quantidade; i++) {
     is->atualizar();
     // adiciona os valores a Serie
-    ser->adicionar(getValor(canalX), getValor(canalY));
+    ser->adicionar(is->getValor(canalX), is->getValor(canalY));
   }
 
   cout << "Imprimindo os pontos obtidos" << endl;
   ser->imprimir();
+  cout << endl;
 
   cout << "Limite Superior: ";
-  cout << "(" << ser->PontoSuperior->getX();
-  cout << ", " << ser->PontoSuperior->getY();
+  cout << "(" << ser->getLimiteSuperior()->X;
+  cout << ", " << ser->getLimiteSuperior()->Y;
   cout << ")" << endl << endl;
 
   cout << "Limite Inferior: ";
-  cout << "(" << ser->PontoInferior->getX();
-  cout << ", " << ser->PontoInferior->getY();
+  cout << "(" << ser->getLimiteInferior()->X;
+  cout << ", " << ser->getLimiteSuperior()->Y;
   cout << ")" << endl;
   return 0;
 }
